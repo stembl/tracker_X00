9ee73a663d3b5cce7f6d2fd20facf163b4022ea0
Steve Embleton <embleton@gmail.com>
2018-07-28 20:36:35 -0500

    added print_accel.py

---
+++ tracker_X00/MPU6050_cal.py
@@ -0,0 +1,27 @@
+# tracker_X00/accel_MPU6050.py
+
+import time
+from mpu6050 import mpu6050
+import datetime
+import math
+
+
+def MPU6050_cal():
+    """
+    Assumes accelerometer connection.
+    Pulls data from MPU6050 accelerometer and performs calculates
+    the ...
+
+    Input
+    -----
+
+
+    Returns
+    -------
+    Calibration results ...
+
+    Examples
+    --------
+    >>> MPU6050_call()
+    Results : XXX
+    """
---
+++ tracker_X00/print_accel.py
@@ -0,0 +1,54 @@
+from time import sleep
+from mpu6050 import mpu6050
+
+def print_accel():
+    """
+    Pull data from the MPU6050 and print the results.
+
+    !!! add set gyro and g range !!!
+
+    Input
+    -----
+    Number of Prints
+
+    Returns
+    -------
+
+    Examples
+    --------
+    >>> print_gps(3)
+
+
+    Accelerometer Data [g]
+    x:
+    y:
+    z:
+
+    Gyroscope Data
+    x:
+    y:
+    z:
+
+    Temp [C]: 20
+    """
+
+
+sensor = mpu6050(0x68)
+
+while True:
+    accel_data = sensor.get_accel_data(g=True)
+    gyro_data = sensor.get_gyro_data()
+    temp = sensor.get_temp()
+
+    print("\n\nAccelerometer Data [g]")
+    print("x: " + str(accel_data['x']))
+    print("y: " + str(accel_data['y']))
+    print("z: " + str(accel_data['z']))
+
+    print("\nGyroscope Data")
+    print("x: " + str(gyro_data['x']))
+    print("y: " + str(gyro_data['y']))
+    print("z: " + str(gyro_data['z']))
+
+    print("\nTemp [C]: " + str(temp) + " C")
+    sleep(0.5)
